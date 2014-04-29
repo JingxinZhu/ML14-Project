913e97aa7cbe8afc07b0639deb6dbf6853bd03e8
Jingxin Zhu <jz1371@nyu.edu>
2014-04-29 11:29:21 -0400

    Create model_svm.py file

---
+++ Code/jz_svm/model_svm.py
@@ -0,0 +1,12 @@
+'''
+File: model_svm.py
+--------------------------------------------------------------------
+This code is a part of the final project for course Machine Learning 
+and Computational Statistics at NYU for Fall 2014.
+
+This file implements SVM method.
+
+Author: Bowei Zhang and Jingxin Zhu
+Date created: 29th, Apr
+--------------------------------------------------------------------
+'''
