7c92fafba06d510c11d8e3194b26521262e76ffb
Jingxin Zhu <jz1371@nyu.edu>
2014-04-30 19:17:15 -0400

    svm_04/30

--- Code/jz_svm/feature_vector.py
+++ Code/jz_svm/feature_vector.py
@@ -15,19 +15,35 @@ class Feature_Vector:
 	def process(self, row = None):
 		try:
 			dim = 22
-			vec = [False] * dim
-
+			vec = [0] * dim
+			
+			# 0 - retweet count
 			vec[0] = int(row[0])
-			vec[1] = row[1]
+
+			# name of user
+			#vec[1] = row[1]
+
+			# 2 - account year
 			vec[2] = int(row[2])
+			# 3 - background picture enabled?
 			vec[3] = self.label(row[3])
+			# 4 - number of lists the user is in
 			vec[4] = int(row[4])
+			# 5 - number of followers the user has 
 			vec[5] = int(row[5])
-			vec[6] = row[6]
+
+			# 6 - the language s/he is using
+			#vec[6] = row[6]
+
+			# 7 - geographical displayed?
 			vec[7] = self.label(row[7])
+			 # 8 - verified?
 			vec[8] = self.label(row[8])
+			# 9 - number of tweets
 			vec[9] = int(row[9])
+			# 10 - number of followers
 			vec[10] = int(row[10])
+			# 11 - 17 - day it is tweeted
 			vec[11] = self.label(row[11])
 			vec[12] = self.label(row[12])
 			vec[13] = self.label(row[13])
@@ -35,9 +51,15 @@ class Feature_Vector:
 			vec[15] = self.label(row[15])
 			vec[16] = self.label(row[16])
 			vec[17] = self.label(row[17])
-			vec[18] = int(row[18])
+
+			# 18 - timestampe when it was tweeted, UTC +0000 
+			#vec[18] = int(row[18])
+
+			# 19 - number of url includede in the tweet
 			vec[19] = int(row[19])
+	    	# 20 - number of hashtags used in the tweet
 			vec[20] = int(row[20])
+			# 21 - number of other users mentioned
 			vec[21] = int(row[21])
 			return vec
 		except:
--- Code/jz_svm/model_svm.py
+++ Code/jz_svm/model_svm.py
@@ -11,6 +11,8 @@ Date created: 29th, Apr
 --------------------------------------------------------------------
 '''
 
+# 1. do not use most recent five because they might be active
+
 import sys
 import csv
 import numpy as np
@@ -18,24 +20,88 @@ from sklearn import svm
 
 import feature_vector as fv
 
-# initialize raw data
-feature = fv.Feature_Vector()
-# use vec to store the results
-vec = []
-# data source
-datafile = '../data/tweets'
 
 # 1. read in tweets from one user, process each tweet into a feature vector
-with open(datafile, 'rb') as f:
-    reader = csv.reader(f, delimiter = ',')
-    try:
-        for row in reader:
-            vec.append(feature.process(row))
-    except csv.Error as e:
+def read_tweets():
+    # initialize raw data
+    feature = fv.Feature_Vector()
+    # use vec to store the results
+    # data source
+    datafile = '../data/tweets'
+    vec = []
+    with open(datafile, 'rb') as f:
+        reader = csv.reader(f, delimiter = ',')
+        try:
+            for row in reader:
+                vec.append(feature.process(row))
+        except csv.Error as e:
             sys.exit('file %s, line %d: %s',  (filename, reader.line_num, e))
+    return vec
 
-# 2. label each feature vector:
+# 2. label each feature vector: after deleting the five hightest and five
+#    lowest retweets, 
 #    use 70% of highest number of retweets as indicator, 
 #    label a feature vecotor as 1 if vector's number of retweets is more than 70%
 #    label it as 0 otherwise.
+def label_vector(vec, USER_SIZE, TWEETS_PER_USER):
+    labels = []
+    matrix = []
+    vec_matirx = np.array(vec)
+    filter_size = 5
+
+    for num in range(USER_SIZE):
+        # select tweets for each user
+        user_tweets = vec_matirx[TWEETS_PER_USER*num : TWEETS_PER_USER+TWEETS_PER_USER*num,:]
+        col_retweet = user_tweets[:,0]
+
+        # find 5 highest and 5 lowest retweets.
+        max_5 = np.argsort(col_retweet)[-filter_size:]
+        min_5 = np.argsort(col_retweet)[:filter_size]
+
+        user_tweets = np.delete(user_tweets,[max_5,min_5],0)
+        col_retweet = user_tweets[:,0]
+
+        # Option 1: 70% of max_after_deleted_top_5
+        #indicator = 0.7 * max(col_retweet)
+        # Option 2: 70% (max_after_deleted_top_5 - min_after_deleted_down_5)
+        indicator = 0.7 * (max(col_retweet) - min(col_retweet))
+
+        # label each feature vector
+        for v in user_tweets:
+            if v[0] > indicator:
+                labels.append(1)
+            else:
+                labels.append(0)
+        matrix.append(user_tweets)   
+    labels = np.array(labels)
+    #print sum(l == 1 for l in labels)
+    print matrix
+    return labels, matrix
+
+# 3. normalize column by column
+def normalize(vec_matirx):
+    X = np.zeros(vec_matirx.shape)
+    for i in range(len(vec_matirx)):
+        col = vec_matirx[:,i]
+        col_max = max(col)
+        col_min = min(col)
+        #if col_max * col_min == 0:
+            #X.append(norm_column(vec_matirx[:,i]))
+
+
+def norm_column(col):
+    return  0
+
+# ------------------- #
+#    Main function    #
+# ------------------- #
+USER_SIZE       = 2
+TWEETS_PER_USER = 200
+vec = read_tweets()
+[labels, vec_matirx] = label_vector(vec, USER_SIZE, TWEETS_PER_USER)
+#X = normalize(vec_matirx) 
+
+#clf = svm.SVC()
+#clf.fit(vec_matirx, labels)
+
 
