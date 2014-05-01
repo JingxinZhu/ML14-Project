f16c17b2db0d8c900d6f299fc8c67ff531ff0ed5
Claude Zhang <zbw0517@gmail.com>
2014-05-01 19:30:33 -0400

    Test comment.

--- Code/src/crawl.py
+++ Code/src/crawl.py
@@ -52,6 +52,7 @@ def main(args):
     for i, t in enumerate(tweets):
         vec.append(cleaner.clean(t))
 
+    # write to the disk
     with open(out_path + '/tweets', 'wb') as f:
         writer = csv.writer(f)
         writer.writerows(vec)
