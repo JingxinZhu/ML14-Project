147a747de11bfb2b608d78782f90a14800b37d64
Jingxin Zhu <jz1371@nyu.edu>
2014-05-05 13:30:00 -0400

    delete kmeans.py

--- Code/src/kmeans.py
+++ Code/src/kmeans.py
@@ -1,17 +1,23 @@
 '''
 File: 
 '''
+import numpy as np
+from sklearn.decomposition import PCA
 
-# Part I: initialization
 import feature_normalizer_new as fn
 
-n_ids = 4
-n_tweets_per_id = 50
+# Part I: initialization
+
+n_ids = 15
+n_tweets_per_id = 150 
+n_feature = 22
 norm = fn.Feature_Normalizer()
 #[labels, X] = norm.normalize(n_ids, n_tweets_per_id)
 [labels, X] = norm.multiclass_normalize(n_tweets_per_id, 4)
 
-# Part II: PCA analysis
-m = len(X)
-print m
 
+# Part II: PCA analysis
+def PCA_analysis(X):
+	U,s,V = np.linalg.svd(X, full_matrices = True)
+	S = np.zeros((len(X), n_feature))
+	S[:n_feature, :n_feature] = np.diag(s)
